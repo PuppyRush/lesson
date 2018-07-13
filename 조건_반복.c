4ad7d877c55d6b2eb9465010a72e257dc8dffaf7
Chaed <gooddaumi@gmail.com>
2018-07-14 00:49:15 +0900

    Initial commit

---
+++ 조건_반복.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+	//for와 while의 차이점..?
+
+	//nested for 에서 유용한 방법..?
+
+	//goto와 스파게티.
+
+	//if(x=0)... 간결성과 가독성
+
+	/*for (int i = 0; i < 10; i++);
+		printf("pp");*/
+
+	// 네모 만들어보기
+
+	//http://www.codeup.kr/JudgeOnline/problem.php?id=1203
+	//if switch do 이용하여 풀어보자.
+
+	//피보나치 만들어보기
+	//1 1 2 3 5 .... n항까지!
+}
