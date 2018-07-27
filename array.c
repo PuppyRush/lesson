b00bc235391ba5ba16e6886fde8dd3db41f69d06
Chaed <gooddaumi@gmail.com>
2018-07-28 01:21:17 +0900

    lesson4

---
+++ array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a[3];
+	double decimal[100];
+
+	int ary2[10][10];
+	
+	printf("%u", &a);
+
+	
+}
---
+++ pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a = 10;
+	printf("%x", &a); 
+
+	int *ptr = &a;
+	
+	int *ptr2 = NULL;
+	//ptr2 = ?
+	//*ptr2 = ?
+
+	
+	//swap 함수 만들어보기 (call by value, call by pointer)
+
+
+	//32bit에서 왜 4gb만 사용 가능?
+	//32bit에선 몇개의 주소 공간이 사용 가능할까
+
+}
