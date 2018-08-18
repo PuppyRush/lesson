2ef2f8006aa237d55cf3c34fed0d8b8f53af672f
Chaed <gooddaumi@gmail.com>
2018-08-18 23:23:00 +0900

    update

--- pointer2.cpp
+++ pointer2.cpp
@@ -1,6 +1,16 @@
 //#include <stdio.h>
 //#include <memory>
 //
+//void set(int ary[10])
+//{
+//
+//}
+//
+//void call_by_ptr(int *ptr, int size)
+//{
+//	//ptr[1]...
+//}
+//
 //int main()
 //{
 //	int integerArray[10];
@@ -26,9 +36,9 @@
 //	int **ptr2 = &ptr;
 //	printf("%p %p\n", ptr2, &ptr);
 //
-//
+//	//memset
 //	int ary2[10][10];
-//	memset(ary2, 0, sizeof(int) * 10 * 10);
+//	memset(ary2, 1, sizeof(int) * 10 * 10);
 //
 //	int number = 100;
 //	for (int i = 0; i < 10; i++)
@@ -53,4 +63,19 @@
 //	//lvalue , rvalue
 //
 //	//malloc
+//	int *heap = (int *)malloc(sizeof(int) * 100);
+//	heap[2] = 3;
+//
+//	for (int i = 10; i < 20; i++)
+//		*(heap+i) = i;
+//
+//	int cpy[10];
+//	memcpy(cpy, heap+10 , sizeof(int) * 10);
+//
+//	free(heap);
+//
+//	
+//
+//
+//	//heap, stack, Code, Data
 //}
--- string.c
+++ string.c
@@ -3,13 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
 
-	char str[100];
-	memset(str, 0, 100);
+	//문자열 초기화
+	
+	//1
+	char *str1 = "Hello World";
 
-	scanf("%s", str);
+	//2
+	char str2[100];
+	memset(str2, 0, 100);
+	strcpy_s(str2, 100, "Hello World!!!");
 
+	//3
+	char str3[100];
+	scanf("%s", str3);		//&?
+	
+	//문자열 저장 및 순회
 
+	//argc argv
 }
