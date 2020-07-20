7c0f07f17f232f0abe5935240a8fea249b7405cb
Chaed <gooddaumi@gmail.com>
2020-07-20 22:49:52 +0900

    queue

--- vector/vector_list.h
+++ vector/vector_list.h
@@ -1,38 +1,44 @@
-namespace array_list
+#include <stdio.h>
+
+const size_t FULL_SIZE = 1000;
+List* queue = NULL;
+List* head = NULL;
+List* tail = NULL;
+
+typedef struct List
 {
-	typedef struct List
-	{
+	int data;
+	List* rear;
+	List* front;
+};
 
-	};
 
+//새로운 데이터를 q에 추가하고 새로운 노드를 반환한다.
+List* enque(List* q, int data)
+{
 
-	void append()
-	{
+}
 
-	}
+//q에서 노드를 제거하고 그 노드를 반환한다.
+List* deque(List* q)
+{
 
-	void remove()
-	{
+}
 
-	}
+//q에서 dest에 해당하는 값을 갖는 노드가 있으면 해당 노드를 반환하고 존재하지 않으면 NULL을 반환한다.
+List* find(List* q, int dest)
+{
 
-	void insert()
-	{
+}
 
-	}
+//q의 크기를 반환한다.
+size_t size(List* q)
+{
 
-	void find()
-	{
+}
 
-	}
+//q의 노드를 오름차순으로 정렬한다.
+void sort()
+{
 
-	int size()
-	{
-
-	}
-
-	void sort()
-	{
-
-	}
 }
