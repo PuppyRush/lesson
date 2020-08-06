37b8d24f255501b52bc1fa7589732b0d0da42bd8
Chaed <gooddaumi@gmail.com>
2020-08-06 22:11:45 +0900

    game

--- WormGame/GameController.cpp
+++ WormGame/GameController.cpp
@@ -1,1 +1,17 @@
 #include "GameController.h"
+
+GameController::GameController()
+	:m_board{ 20,20 }
+{
+	//g_Board를 초기화한다. (다운/업 캐스팅 개념 학습 필요)
+	//벽을 초기화 한다.
+	m_board.setUnit(new Wall{ 0,0 });
+	m_board.setUnit(new Wall{ 0,1 });
+	m_board.setUnit(new Wall{ 0,2 });
+	//...나머지 벽들도 초기화 한다.
+
+	//지렁이를 초기화 한다.
+	m_board.setUnit(new Worm{ 5,5 });
+
+	//... 
+}
